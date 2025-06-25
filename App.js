421dba2287b4289357b1991cd52f602a2822765c
Ar-ee-ba <areebajhangir0@gmail.com>
2025-06-25 20:07:17 -0300

    feature:folder of screens in src

--- App.js
+++ App.js
@@ -1,57 +1,5 @@
-import { StyleSheet, View, Button, TextInput, Text } from "react-native";
+import Login from "./src/screens/login";
 
 export default function App() {
-  function OnLoginButtonPress() {
-    alert("Welcome!!!");
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Please login into your account</Text>
-
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-
-      <View style={styles.buttonContainer}>
-        <Button onPress={OnLoginButtonPress} title="Login" />
-      </View>
-    </View>
-  );
+  return <Login />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#4c669f",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-
-  heading: {
-    fontSize: 20,
-    color: "white",
-    marginBottom: 30,
-  },
-
-  input: {
-    width: "100%",
-    padding: 15,
-    margin: 10,
-    borderWidth: 1,
-    borderColor: "white",
-    borderRadius: 12,
-    backgroundColor: "white",
-    color: "black",
-  },
-
-  buttonContainer: {
-    marginTop: 20,
-    width: "90%",
-    borderRadius: 12,
-  },
-});
