8d68b41175cb3f19365ece95117ca257d1b7f384
Ar-ee-ba <areebajhangir0@gmail.com>
2025-06-29 19:07:16 -0300

    feature:Counter

--- src/screens/counter.js
+++ src/screens/counter.js
@@ -1,5 +1,6 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useState } from "react";
+import Button from "../components/button";
 
 const ranger = 1;
 const MIN_COUNT_LIMIT = 0;
@@ -31,17 +32,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E0F7FA",
+    paddingHorizontal: 10,
+    paddingVertical: 20,
   },
   box: {
-    width: "100%",
-    height: "30%",
+    width: "90%",
+    height: 150,
     flexDirection: "row",
-    backgroundColor: "orange",
-    gap: 10,
+    backgroundColor: "#ADD8E6",
+    borderRadius: 10,
+    padding: 10,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
   },
   counterText: {
-    fontSize: 40,
+    fontSize: 45,
+    fontWeight: "bold",
+    color: "#000080",
   },
 });
